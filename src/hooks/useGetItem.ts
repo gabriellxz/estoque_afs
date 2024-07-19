6292ab5938bda9635289fc2ba8cf8d8fc7cea39a
araujoedu7 <ea144027@gmail.com>
2024-07-19 00:18:24 -0300

    add hook personalizado de getItem

---
+++ src/hooks/useGetItem.ts
@@ -0,0 +1,45 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthUser } from "../context/authContext";
+import useGetAllComponentes from "../hooks/useGetAllComponentes";
+import { itemCompany } from "../types/itemCompany";
+import api from "../config/config";
+
+export default function useGetItem() {
+
+    const { token } = useContext(AuthUser);
+    const { componente } = useGetAllComponentes();
+    const [items, setItems] = useState<itemCompany[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        async function getItems() {
+
+            setLoading(true);
+
+            try {
+                if (token) {
+                    const response = await api.get("/Item", {
+                        headers: {
+                            "Authorization": "Bearer " + JSON.parse(token)
+                        }
+                    })
+
+                    // console.log(response);
+                    setItems(response.data.Company);
+                }
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        getItems();
+    }, [token])
+
+    return {
+        componente,
+        items,
+        loading
+    }
+}

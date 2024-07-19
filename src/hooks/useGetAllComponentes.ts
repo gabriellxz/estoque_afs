ee4eb43020afe2d7862f466efcd90977997d981d
araujoedu7 <ea144027@gmail.com>
2024-07-19 00:17:55 -0300

    alteração no get de componentes

--- src/hooks/useGetAllComponentes.ts
+++ src/hooks/useGetAllComponentes.ts
@@ -1,55 +1,33 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react"
+import { Componente } from "../types/componente";
 import { AuthUser } from "../context/authContext";
 import api from "../config/config";
-
-type ItemType = {
-    nome: string;
-    estoque: number;
-}
-
-type CategoryType = {
-    nome: string;
-    item: ItemType[];
-}
-
-type AllComponentes = {
-    id_component: number;
-    nome_component: string;
-    Category: CategoryType[];
-}
 
 export default function useGetAllComponentes() {
 
     const { token } = useContext(AuthUser);
-    const [componentes, setComponentes] = useState<AllComponentes[]>([]);
-    // const [loading, setLoading] = useState<boolean>(false);
+    const [componente, setComponente] = useState<Componente[]>([]);
 
     useEffect(() => {
-        async function getComponents() {
-            // setLoading(true);
+        async function getComp() {
+            try {
+                if(token) {
+                    const response = await api.get("/Componentes", {headers: {
+                        "Authorization": "Bearer " + token
+                    }})
 
-            if (token) {
-                try {
-                    const response = await api.get("/components", {
-                        headers: {
-                            "Authorization": "Bearer " + JSON.parse(token)
-                        }
-                    })
-                    // setLoading(false)
-                    // console.log(response);
-                    setComponentes(response.data);
-                } catch (error) {
-                    console.log(error);
-                    // setLoading(false)
+                    setComponente(response.data);
+                    console.log(response.data);
                 }
+            } catch (e) {
+                console.log(e);
             }
         }
 
-        getComponents();
-    }, [componentes])
+        getComp();
+    }, [])
 
     return {
-        componentes,
-        // loading
+        componente
     }
 }
